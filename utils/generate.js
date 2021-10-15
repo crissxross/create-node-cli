0a196a98b9d38990fdb577d01f6dd9c6cb4bf529
Christine Wilks <crissxross@gmail.com>
2021-10-15 14:39:14 +0100

    improve UX, loading spinners

--- utils/generate.js
+++ utils/generate.js
@@ -1,3 +1,4 @@
+import ora from 'ora';
 import { fileURLToPath } from 'url';
 import path from 'path';
 import execa from 'execa';
@@ -6,6 +7,7 @@ import copyTemplateDir from 'copy-template-dir';
 import chalk from 'chalk';
 import { questions } from './questions.js';
 
+const spinner = ora({ text: '' });
 // __filename and __dirname are from CommonJS, so create equivalents in ESM
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -14,31 +16,52 @@ const green = chalk.green;
 const dim = chalk.dim;
 
 const generate = async () => {
-  const vars = await questions();
-  const outDir = vars.name;
-  const inDirPath = path.join(__dirname, '../template');
-  const outDirPath = path.join(process.cwd(), outDir);
+	const vars = await questions();
+	const outDir = vars.name;
+	const inDirPath = path.join(__dirname, '../template');
+	const outDirPath = path.join(process.cwd(), outDir);
 
-  copyTemplateDir(inDirPath, outDirPath, vars, async (err, createdFiles) => {
-    if (err) throw err;
+	copyTemplateDir(inDirPath, outDirPath, vars, async (err, createdFiles) => {
+		if (err) throw err;
 
-    console.log(dim(`\nCreating files in ${green(`./${outDir}`)} directory:\n`));
+		console.log(
+			dim(`\nCreating files in ${green(`./${outDir}`)} directory:\n`)
+		);
 
-    createdFiles.forEach((filePath) => {
-      const fileName = path.basename(filePath);
-      console.log(`${green.dim(`CREATED`)} ${fileName}`);
-    });
+		createdFiles.forEach(filePath => {
+			const fileName = path.basename(filePath);
+			console.log(`${green.dim(`CREATED`)} ${fileName}`);
+		});
 
-    process.chdir(outDirPath);
-    // FIXME this dedupe is not working - why?
-    await execa(`npm`, [`dedupe`]);
+		spinner.start(
+			`${chalk.yellow(`DEPENDENCIES`)} installing...\n${dim(
+				`It may take a moment...`
+			)}`
+		);
+		process.chdir(outDirPath);
+		const pkgs = [
+			`meow`,
+			`chalk`,
+			`cli-alerts`,
+			`cli-welcome`,
+			`cli-meow-help`,
+			`cli-handle-error`,
+			`cli-handle-unhandled`
+		];
+		await execa(`npm`, [`install`, ...pkgs]);
+		await execa(`npm`, [`install`, `prettier`, `-D`]);
+		// dedupe wasn't removing repeat line in package.json until I added the npm install command above - why?
+		await execa(`npm`, [`dedupe`]);
+		spinner.succeed(`${green(`DEPENDENCIES`)} installed (& DEDUPE ran)`);
 
-    cliAlerts({
-      type: `success`,
-      name: `ALL DONE`,
-      msg: `\n${createdFiles.length} files created in ${dim(`./${outDir}`)} directory`,
-    });
-  });
+		cliAlerts({
+			type: `success`,
+			name: `ALL DONE`,
+			msg: `\n${createdFiles.length} files created in ${dim(
+				`./${outDir}`
+			)} directory`
+		});
+	});
 };
 
 export { generate };
