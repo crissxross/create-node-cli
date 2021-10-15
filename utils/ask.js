807d162319c12b98df31f29f09dec0095892b343
Christine Wilks <crissxross@gmail.com>
2021-10-15 15:13:36 +0100

    persist history to remember last input

--- utils/ask.js
+++ utils/ask.js
@@ -1,17 +1,40 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
 import enquirerPkg from 'enquirer';
 const { Input } = enquirerPkg; // because it's a CommonJS package
 import { to } from 'await-to-js';
 import cliHandleError from 'cli-handle-error';
 import cliShouldCancel from 'cli-should-cancel';
+import storePkg from 'data-store';
+const { Store } = storePkg;
+
+// __filename and __dirname are from CommonJS, so create equivalents in ESM
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const ask = async ({ name, message, hint, initial }) => {
+	let history = false;
+	if (
+		!initial &&
+		name !== `name` &&
+		name !== `command` &&
+		name !== `description`
+	) {
+		history = {
+			autosave: true,
+			store: new Store({
+				path: path.join(__dirname, `/../.history/${name}.json`)
+			})
+		};
+	}
 	const [err, response] = await to(
 		new Input({
 			name,
 			message,
 			hint,
 			initial,
+			history,
 			validate(value, state) {
 				if (state && state.name === `command`) return true;
 				if (state && state.name === `name`) {
