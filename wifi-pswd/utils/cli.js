f7dc9c0aed8b69af4a8097f03a6b02fdb79b5010
Christine Wilks <crissxross@gmail.com>
2021-10-18 15:48:25 +0100

    adds wifi-pswd cli app

---
+++ wifi-pswd/utils/cli.js
@@ -0,0 +1,46 @@
+import meow from 'meow';
+import meowHelp from 'cli-meow-help';
+
+const flags = {
+	clear: {
+		type: `boolean`,
+		default: true,
+		alias: `c`,
+		desc: `Clear the console`
+	},
+	debug: {
+		type: `boolean`,
+		default: false,
+		alias: `d`,
+		desc: `Print debug info`
+	},
+	version: {
+		type: `boolean`,
+		alias: `v`,
+		desc: `Print CLI version`
+	}
+};
+
+const commands = {
+	help: {
+		description: `Print help info`
+	}
+};
+
+const helpText = meowHelp({
+	name: `wifi-pswd`,
+	flags,
+	commands
+});
+
+const options = {
+	importMeta: import.meta,
+	inferType: true,
+	description: false,
+	hardRejection: false,
+	flags
+};
+
+const cli = meow(helpText, options);
+
+export { cli };
